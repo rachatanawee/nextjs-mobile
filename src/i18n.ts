4e8f76a3c848d2ba427776bccd66b41d980b38d7
Rachatanawee <rachatanawee@csithai.com>
2025-12-04 08:32:35 +0700

    fix message location

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,12 @@
 import {getRequestConfig} from 'next-intl/server';
- 
+
+const en = require('../messages/en.json');
+const th = require('../messages/th.json');
+
 export default getRequestConfig(async ({locale}) => {
   const resolvedLocale = locale ?? 'en';
   return {
     locale: resolvedLocale,
-    messages: (await import(`../messages/${resolvedLocale}.json`)).default
+    messages: resolvedLocale === 'en' ? en : th
   };
 });
